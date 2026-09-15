734979d64be041cebbe0fe22d0025e63d9074c1c
Drew Williams <Andrewguy060806@gmail.com>
2026-09-15 13:46:07 -0500

    Restarted and created a button file

---
+++ to_dont_list/lib/widgets/basic_buttons.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+typedef CalculatorCallback = Function(String value);
+class CalculatorButtons extends StatelessWidget {
+    final CalculatorCallback onButtonPressed;
+    const CalculatorButtons({
+        super.key,
+        required this.onButtonPressed,
+});
+
+    Widget buildButton(String buttonText){
+        return SizedBox(
+            width: 80,
+            height: 60,
+            child: ElevatedButton(
+                onPressed: () => onButtonPressed(buttonText),
+                child: Text(buttonText, style: const TextStyle(fontSize: 24)),
+            ),
+        );
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        return Wrap(
+            spacing: 10.0,
+            runSpacing: 10.0,
+            children: [
+                buildButton('1'),
+                buildButton('2'),
+                buildButton('3'),
+                buildButton('4'),
+                buildButton('5'),
+                buildButton('6'),
+                buildButton('+'),
+                buildButton('-'),
+                buildButton('='),
+            ],
+        );
+    }
+}
